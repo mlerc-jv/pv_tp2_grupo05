9325d0f14f66cecfc9918780e7ac48262136ade5
mlerc-jv <jimenavirmeru@gmail.com>
2026-04-23 15:57:04 -0300

    Archivo JS - Meruvia Jimena

--- src/holaJSMeruviaJimena.js
+++ src/holaJSMeruviaJimena.js
@@ -3,7 +3,11 @@ var nombre = "Jimena";
 //trabajar con let y const
 let apellido = "Meruvia";
 //const TEXTO = "Hola Mundo, según ";
+const TEXTO = ` Hola Mundo, según ${nombre} ${apellido} `;
+//const TEXTO = ` Hola Mundo, según ${nombre} ${apellido} `;
 
 //console.log("Hola Mundo, según " + nombre + apellido);
 
 //console.log(TEXTO + nombre + apellido );
+
+console.log ( TEXTO );
