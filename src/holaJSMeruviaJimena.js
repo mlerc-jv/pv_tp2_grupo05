da196366ca92b5126b0b8b8f468e9e2edf9bc2e4
mlerc-jv <jimenavirmeru@gmail.com>
2026-04-23 15:28:25 -0300

    Primer commit - Meruvia Jimena

---
+++ src/holaJSMeruviaJimena.js
@@ -0,0 +1,9 @@
+//no usar
+var nombre = "Jimena";
+//trabajar con let y const
+let apellido = "Meruvia";
+//const TEXTO = "MENSAJE";
+
+//console.log("Hola Mundo, según " + nombre + apellido);
+
+//console.log(TEXTO + nombre + apellido );
