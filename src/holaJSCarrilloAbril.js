d84d22918e93383f3e285ceed84656e8be1724d7
Carrillo Marianela Valentina Abril <carrillomarianelaabril@gmail.com>
2026-04-23 16:35:30 -0300

    Agregado de holaJSCarrilloAbril.js - CarrilloAbril

---
+++ src/holaJSCarrilloAbril.js
@@ -0,0 +1,12 @@
+//no usar
+var nombre = "Abril";
+//trabajar con let y const
+let apellido = "Carrillo";
+//const TEXTO = "Hola Mundo, según ";
+const TEXTO = ` Hola Mundo, según ${nombre} ${apellido} `;
+
+//console.log("Hola Mundo, según "+ nombre + apellido);
+
+//console.log(TEXTO + nombre + apellido );
+
+console.log ( TEXTO );
