9b0624c5ab60bf424068428ca1463ee014c84e6e
Guadalupe Quispe <guada.bqm@gmail.com>
2026-04-23 17:13:13 -0300

    Agrego Archivo JS - Quispe Guadalupe

---
+++ src/holaJSQuispeGuadalupe.js
@@ -0,0 +1,13 @@
+//no usar
+var nombre = "Guadalupe";
+//trabajar con let y const
+let apellido = "Quispe";
+//const TEXTO = "Hola Mundo, según ";
+const TEXTO = ` Hola Mundo, según ${nombre} ${apellido} `;
+//const TEXTO = ` Hola Mundo, según ${nombre} ${apellido} `;
+
+//console.log("Hola Mundo, según " + nombre + apellido);
+
+//console.log(TEXTO + nombre + apellido );
+
+console.log ( TEXTO );
